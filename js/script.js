4bfcdc8f586eacf7d2cb7fe8bb6c6656eee6c266
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-04-25 18:11:35 +0000

    svsdfvsdf

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function checking() {
+  const usernumber = parseInt(document.getElementById("number").value)
+  if (usernumber >= 17) {
+    document.getElementById("result").innerHTML = "you can see R movie alone"
+  }
+  else if (usernumber >= 13) {
+    document.getElementById("result").innerHTML = "you have can see PG-13 movie alone"
+  }
+  else if (usernumber >= 5) {
+    document.getElementById("result").innerHTML = "you have can see a G or PG movie alone"
+  }
+  else {
+    document.getElementById("result").innerHTML = "you are too young for most thing"
+  }
+}
